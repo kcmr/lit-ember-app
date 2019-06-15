0f1ee440ad892908b36fba5b48fe5ca0255b56f5
Kus Cámara <kcmr@users.noreply.github.com>
2019-06-15 21:00:41 +0200

    move tree processing to an in-repo addon

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,31 +1,9 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const Funnel = require('broccoli-funnel');
-const MergeTrees = require('broccoli-merge-trees');
-const Rollup = require("broccoli-rollup");
-const rollupConfig = require('./config/rollup-bundler-config');
 
 module.exports = function(defaults) {
   const app = new EmberApp(defaults);
 
-  const litElementsFunnel = new Funnel('app/lit-elements', {
-    annotation: 'Lit Elements folder',
-    include: ['**/*.js'],
-    destDir: 'lit-elements'
-  });
-
-  const entryPointFunnel = new Funnel('app', {
-    annotation: 'Rollup entry point',
-    files: ['lit-elements-imports.js']
-  });
-
-  const mergedTree = MergeTrees([
-    litElementsFunnel,
-    entryPointFunnel
-  ]);
-
-  const bundle = new Rollup(mergedTree, rollupConfig(app));
-
-  return new MergeTrees([bundle, app.toTree()]);
+  return app.toTree();
 };
